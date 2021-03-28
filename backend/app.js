67e07524c1ae201d3e1c864b35e05080573ca875
TheDryck <48965636+TheDryck@users.noreply.github.com>
2021-03-28 14:22:31 -0500

    Added product and order routes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(express.json());
 
-var app = express();
+//Import routes
+const productsRoute = require('./routes/products');
+const ordersRoute = require('./routes/orders');
+
+//Use routes
+app.use('/api/products', productsRoute);
+app.use('/api/orders', ordersRoute);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors({
+  origin: "*",
+  methods:['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+  allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +33,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/*app.use('/', indexRouter);
+app.use('/users', usersRouter);*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
